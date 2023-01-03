2f6a86c028acb267202a52997ac39f7eea29c2aa
Ducpham <ducpv@beetsoft.com>
2023-01-03 14:17:26 +0700

    update logic edit curd

--- common/js/main.js
+++ common/js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
+ 
   $(".js-add").click(function() {  
     let valueQr = $("#qr").val();
     let valueName = $("#name").val(); 
     let valuePrice = $("#price").val(); 
-    
     //function load_ajax()-> add values in Data api
     function load_ajax_add() {
       //post value ajax api
@@ -15,8 +15,8 @@ $(document).ready(function() {
           name: valueName,
           price: valuePrice
         },
-        success: function (data){
-          // Note: Mỗi lần add data-> reload lại trang-> update data mới
+        success: function (data){          
+          // Note: Click add Value  -> reload page -> render page have new data
             window.location.reload();
          }
       });
@@ -27,25 +27,81 @@ $(document).ready(function() {
   //Note:  $(document).on('click','.js-delete',function(){  }); -> luôn luôn có thể click đến đối tượng mình muốn bắt sự kiện -> không cần quan tâm là đã đc sinh ra  class đó hay chưa (có những class đc sinh ra khi chạy hêt một sự kiện vdu : ajax -> call -> sinh ra class )
   // - $("").click(function() {  } -> chỉ chạy đc khi mà đã có sẵn class cố định(thường là fix cố định ở html ) -> cái đổi tượng mà mình muốn bắt skien 
 
-  
+  // Delete value
   $(document).on('click','.js-delete',function(){
-    let idValue= $('.js-delete').attr('id')    
+    let idDelete= $(this).attr('id');  
+    console.log(idDelete);
     $.ajax({
-      url: `https://63a56082318b23efa791bf88.mockapi.io/api/crud/${idValue}`,
+      url: `https://63a56082318b23efa791bf88.mockapi.io/api/crud/${idDelete}`,
       type: 'DELETE',
       success: function(data) {        
-        window.location.reload();
-        // console.log(data)
+        window.location.reload();     
       }
     });
-    // console.log("hih");
   });
+
+  // Edit value
+  $(document).on('click','.js-change',function(){
+    let valueQr = $("#qr").val();
+    let valueName = $("#name").val(); 
+    let valuePrice = $("#price").val(); 
+    let idChange = $(this).attr('id');
+    // console.log(idChange,"change");
+
+    // render data vào lại ô input nhập
+    $.ajax({
+      type: "GET",
+      url: "https://63a56082318b23efa791bf88.mockapi.io/api/crud",
+      success: function(data){
+          // console.log(idChange);          
+          $.each(data, function( index, value ) {
+            // console.log(value.id);
+            if( idChange == value.id){
+              let valueInput = `
+              <div class="form_value">
+                <div class="fied">
+                  <label for="">Ma Sp</label>
+                  <input type="text" name="qr" id="qr" value="${value.qr}">
+                </div>
+                <div class="fied">
+                  <label for="">Ten</label>
+                  <input type="text" name="name" id="name"  value="${value.name}">
+                </div>
+                <div class="fied">
+                  <label for="">Gia</label>
+                  <input type="number" name="price" id="price"  value="${value.price}">
+                </div>
+              </div>
+              <button class="add_item--btn js-add">Add</button>
+              `;  
+              $(".form_value").empty();                      
+              $(".form_value").append(valueInput);            
+            }             
+          });  
+        }
+    });
+
+    $.ajax({
+      type: "POST",
+      url: `https://63a56082318b23efa791bf88.mockapi.io/api/crud/${idChange}`,
+      data: {
+        qr: valueQr,
+        name: valueName,
+        price: valuePrice
+      },
+      success: function (data){   
+        console.log(valueQr);                    
+      }
+    });
+  });
+
+
+
 
   //Call data Api 
   $.ajax({
     type: "GET",
     url: "https://63a56082318b23efa791bf88.mockapi.io/api/crud",
-    // data: {"data":"check"},
     success: function(data){
       renderData(data);
       console.log(data);      
@@ -61,8 +117,8 @@ $(document).ready(function() {
         <td>${value.name}</td>
         <td>${value.price}</td>
         <td class="action">
-          <span class="js-change">Sửa</span>
-          <span class="js-delete" id="${value.id}" >Xóa</span>
+          <span class="js-change" id="${value.id}">Sửa</span>
+          <span class="js-delete" id="${value.id}">Xóa</span>
         </td>                  
       </tr>`;  
       $(".table .tbody").append(dataShow);
